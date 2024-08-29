7c2b29cef4e21ac111ddf33bb0f6b33287ff2b58
Carlos Augusto <carlos.fernandes@ifrj.edu.br>
2024-08-29 17:23:22 -0300

    html, css com php

--- consulta.php
+++ consulta.php
@@ -6,7 +6,7 @@
     $resultado = mysqli_query($conexao, $sql);
     if (mysqli_num_rows($resultado) > 0) {
         while($registro = mysqli_fetch_assoc($resultado)) {
-            echo "Id: " . $registro["id"]. " - Nome: " . $registro["nome"]. " " . $registro["senha"]. " - Email: " . $registro["email"]. "<br>";
+            echo "<p style='background-color: lightgreen';>Id: " . $registro["id"]. " - Nome: " . $registro["nome"]. " " . $registro["senha"]. " - Email: " . $registro["email"]. "</p>";
         }
     } else {
         echo "Nenhum registro encontrado.";
@@ -37,7 +37,7 @@
     }
   
     echo "<br><b>Verificar se existe usuário='Carlos' com senha='123'e mostrar seu email. Caso não exista, mostrar uma mensagem.</b><br>";
-    $sql = "SELECT id, nome, senha, email FROM usuario where nome='Carlos' and senha='123'";
+    $sql = "SELECT email FROM usuario where nome='Carlos' and senha='123'";
     $resultado = mysqli_query($conexao, $sql);
     echo "<br>";
     if (mysqli_num_rows($resultado) > 0) {
---
+++ usuarios.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        ul {
+            display: flex;
+            /* flex-direction: row; */
+            list-style: none;
+            background-color: black;
+        }
+
+        li {
+            padding: 5px;
+        }
+
+        * {
+            margin: 0;
+        }
+
+        a {
+            text-decoration: none;
+            color: white;
+        }
+
+        a:hover {
+            background-color: white;
+            color: black;
+            padding: 8px;
+            transition: 250ms;
+        }
+        main{
+            display: flex;
+            margin: 10px;
+            padding: 10px;
+        }
+        img{
+            width: 120px;
+        }
+        #imagem{
+            /* background-color: lightblue; */
+            /* width:"20%"; */
+            /* height:"20%"; */
+        }
+        #dados{
+            background-color: lightcoral;
+            width: 80%;
+            height: 80%;
+            color: green;
+            border: 2px solid;
+            border-radius: 10px;
+            padding: 10px;
+        }
+        span{
+            color: black;
+            font-weight: bold;
+            border-bottom: 1px solid;
+            margin: 15px;
+        }
+        p{
+            background-color: palegreen;
+            /* border: 1px dashed; */
+            padding: 15px;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Contatos</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div id="imagem">
+            <img src="imagem.jpg">
+        </div>
+        <div id="dados">
+            <p>
+            <?php
+                include "conecta.php";
+                $sql = "SELECT id, nome, senha, email FROM usuario";
+                $resultado = mysqli_query($conexao, $sql);
+                if (mysqli_num_rows($resultado) > 0) {
+                    while($registro = mysqli_fetch_assoc($resultado)) {
+                        echo "<span> ID: </span>". $registro["id"];
+                        echo "<span> Nome: </span>". $registro["nome"];
+                        echo "<span> E-mail: </span>". $registro["email"];
+                        echo "<br>";
+                    }
+                }
+            ?>
+            </p>
+        </div>
+    </main>
+
+</body>
+
+</html>
